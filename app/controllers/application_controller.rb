4beb0f5ce072a9c4d44aec553b366ede5aa67324
Himalayan-wildcat <npng.h.0709@gmail.com>
2018-06-04 16:07:43 +0900

    edit controllers for indent to be activated correctly in JSON format

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def pretty_json(obj, options = nil)
+    render json: JSON.pretty_generate(obj.as_json(options))
+  end
+  
 end
--- app/controllers/history_controller.rb
+++ app/controllers/history_controller.rb
@@ -1,6 +1,8 @@
 class HistoryController < ApplicationController
+
   def index
-    @chats = Chat.order("created_at DESC").limit(10)
-    render 'index', formats: 'json', handlers: 'jbuilder'
+    @chats = Chat.order("created_at DESC").limit(10).select(:id, :user_input, :bot_response, :created_at)
+    pretty_json @chats
   end
+
 end
