29bba8e2a5e0851c71111d36c093ad7dbe310de6
Himalayan-wildcat <npng.h.0709@gmail.com>
2018-06-05 13:10:42 +0900

    modify history controller

--- app/controllers/history_controller.rb
+++ app/controllers/history_controller.rb
@@ -1,7 +1,8 @@
 class HistoryController < ApplicationController
 
   def index
-    @chats = Chat.order("created_at DESC").limit(10).select(:id, :user_input, :bot_response, :created_at)
+    @chats = Chat.order("created_at DESC").limit(10).select(:user_input, :bot_response, :created_at).as_json(except: :id)
+    # to_json returns String.  as_json returns Hash with String keys.
     pretty_json @chats
   end
 
